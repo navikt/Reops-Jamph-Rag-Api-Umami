f2e0ba5923d3ea746eb8a0ad40e37fbf4e45177b
PerErikGronvik <pererik87@gmail.com>
2026-03-18 12:39:16 +0100

    test was testing the valid sql function that is already tested. added a test to thest this function.

--- src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
+++ src/test/kotlin/no/jamph/llmValidation/EstimateCostSqlTest.kt
@@ -1,13 +1,26 @@
 package no.jamph.llmValidation
 
+import com.google.cloud.bigquery.*
+import io.mockk.every
+import io.mockk.mockk
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class EstimateCostSqlTest {
-    
+
     @Test
-    fun `invalid SQL returns 0`() {
-        assertEquals(, actual)
+    fun `returns correct MB estimate`() {
+        val mockStats = mockk<JobStatistics.QueryStatistics> {
+            every { totalBytesProcessed } returns (1024L * 1024L * 10L) // 10 MB
+        }
+        val mockJob = mockk<Job> {
+            every { getStatistics<JobStatistics.QueryStatistics>() } returns mockStats
+        }
+        val mockBigQuery = mockk<BigQuery> {
+            every { create(any<JobInfo>()) } returns mockJob
+        }
+
+        val result = estimateCostInMB("SELECT 1", mockBigQuery)
+        assertEquals(10.0, result)
     }
 }
